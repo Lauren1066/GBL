b80e34049b7b3d97ca70ca263b7de072c8012261
Lauren <61550272+Lauren1066@users.noreply.github.com>
2023-02-12 13:48:07 -0500

    Added Restart command

---
+++ Commands/Staff/restart.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const axios = require("axios");
+const config = require("../../Storage/config.json");
+module.exports = {
+  data: new SlashCommandBuilder().setName("restart").setDescription("Restart the bot").setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+  async execute(interaction) {
+    await interaction.reply("Attempting to restart...");
+    const url = "https://control.sparkedhost.us/api/client/servers/d8d3c9bf/power?signal=restart";
+
+    axios({
+      method: "POST",
+      url: url,
+      headers: {
+        Authorization: "Bearer " + config.apiKey,
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        console.log(`${response.status}: ${response.statusText}`);
+      })
+      .catch(function (error) {
+        console.log(`${error.code}`);
+        interaction.reply("An error occured!");
+      });
+  },
+};
