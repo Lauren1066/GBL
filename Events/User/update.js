d7122f66247701f78999f9863146c5d633e6c768
Lauren <61550272+Lauren1066@users.noreply.github.com>
2023-02-12 13:36:13 -0500

    Fix UserUpdate Event

--- Events/User/update.js
+++ Events/User/update.js
@@ -6,22 +6,20 @@ module.exports = {
   name: "userUpdate",
   once: false,
   async execute(oldUser, newUser) {
-    if (oldUser.guild.id == constantsfile.mainServerID) {
-      var logsChannel = await oldUser.guild.channels.fetch(constantsfile.mainLogsChannel);
-    } else if (oldUser.guild.id == constantsfile.ambulanceServerID) {
-      var logsChannel = await oldUser.guild.channels.fetch(constantsfile.ambulanceLogsChannel);
-    } else if (oldUser.guild.id == constantsfile.fireServerID) {
-      var logsChannel = await oldUser.guild.channels.fetch(constantsfile.fireLogsChannel);
-    } else if (oldUser.guild.id == constantsfile.policeServerID) {
-      var logsChannel = await oldUser.guild.channels.fetch(constantsfile.policeLogsChannel);
-    }
+    const mainLogsChannel = await oldUser.guild.channels.fetch(constantsfile.mainLogsChannel);
+    const ambulanceLogsChannel = await oldUser.guild.channels.fetch(constantsfile.ambulanceLogsChannel);
+    const fireLogsChannel = await oldUser.guild.channels.fetch(constantsfile.fireLogsChannel);
+    const policeLogsChannel = await oldUser.guild.channels.fetch(constantsfile.policeLogsChannel);
 
     if (oldUser.username != newUser.username) {
       const embed = new EmbedBuilder()
         .setTitle("Username Edited")
         .addFields({ name: "Original Name:", value: oldUser.username }, { name: "New Name:", value: newUser.username })
         .setColor("#2980B9  ");
-      logsChannel.send({ embeds: [embed] });
+      mainLogsChannel.send({ embeds: [embed] });
+      ambulanceLogsChannel.send({ embeds: [embed] });
+      fireLogsChannel.send({ embeds: [embed] });
+      policeLogsChannel.send({ embeds: [embed] });
     }
   },
 };
